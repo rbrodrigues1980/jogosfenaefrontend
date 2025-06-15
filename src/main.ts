41e5b18ecfaecfeacd9a61af528545615f447601
Rogerio Rodrigues <rogeriorodrigues@fenae.org.br>
2025-06-14 23:43:43 -0300

    fix(home): update title to include 'e Apcefs' for consistency fix(main): add missing ts-ignore comments for clarity

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+// @ts-ignore
 import { appConfig } from './app/app.config';
+// @ts-ignore
 import { App } from './app/app';
 
 registerLocaleData(localePt, 'pt-BR');
