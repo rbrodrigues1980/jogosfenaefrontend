c079fd043fd6883c572319b61ccd06c37ca57783
Rogerio Rodrigues <rbrodrigues@gmail.com>
2025-06-15 19:24:29 -0300

    Add company management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { EditionComponent } from './edition/edition';
+import { CompanyComponent } from './company/company';
 import { HomeComponent } from './home/home';
 
 export const routes: Routes = [
   { path: 'edition', component: EditionComponent },
+  { path: 'company', component: CompanyComponent },
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/company/company-form-dialog.ts
@@ -0,0 +1,63 @@
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CompanyDto } from './company-api';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-company-form-dialog',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './company-form-dialog.html',
+  styleUrls: ['./company-form-dialog.css']
+})
+export class CompanyFormDialogComponent {
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CompanyFormDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { company?: CompanyDto },
+    private logger: LoggingService
+  ) {
+    this.form = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+      participantNumber: [0, Validators.required],
+      presidentNumber: [0, Validators.required],
+      sportsDirectorNumber: [0, Validators.required],
+      athleteNumber: [0, Validators.required],
+      parathleteNumber: [0, Validators.required],
+      technicalNumber: [0, Validators.required],
+      editionId: ['', Validators.required]
+    });
+
+    if (data.company) {
+      this.form.patchValue({ ...data.company, editionId: data.company.edition?.id });
+    }
+  }
+
+  cancel() {
+    this.logger.log('cancel company dialog');
+    this.dialogRef.close();
+  }
+
+  save() {
+    if (this.form.valid) {
+      this.logger.log('save company dialog', this.form.value);
+      this.dialogRef.close(this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+}
